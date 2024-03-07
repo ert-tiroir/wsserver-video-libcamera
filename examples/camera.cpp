93beb9656d5a2249d6638fb4113dbf5151234842
Mr Thimote <mrthimote@gmail.com>
2024-03-07 10:23:02 +0100

    1.x: libcamera try 2

--- examples/camera.cpp
+++ examples/camera.cpp
@@ -6,31 +6,28 @@
 #include <wsvideo-ffmpeg/subprocess.h>
 
 WebSocketServer server;
+VideoBroadcaster broadcaster(nullptr, "");
 
 CallbackMiddleware libcamera;
 FFMPEGMiddleware   ffmpeg;
 
 void onCameraData (std::string data) {
-    ffmpeg.send(data);
+    broadcaster.sendPacket(data);
 }
 
 int main () {
     server.init(5420);
-    VideoBroadcaster broadcaster = VideoBroadcaster(&server, "flux.mp4");
+    broadcaster = VideoBroadcaster(&server, "flux.mp4");
 
     while (!server.listen()) continue ;
 
     auto u = onCameraData;
 
     libcamera = CallbackMiddleware();
-    libcamera.init( subprocess( "libcamera-vid", "libcamera-vid", "-t", "0", "-o", "-" ), u );
-
-    ffmpeg.init(&broadcaster);
-    ffmpeg.getPipe()->useBlocking(false);
+    libcamera.init( subprocess( "sh", "sh", "-c", "libcamera-vid -t 0 -o - | ffmpeg -re -i - -g 50 -movflags frag_keyframe+empty_moov+default_base_moof -f mp4 output.mp4" ), u );
 
     while (true) {
         libcamera.tick();
-        ffmpeg   .tick();
 
         broadcaster.tick();
     }
